057aaa111cea205b0bb6ea74f85098cbceabfc38
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-02-22 11:12:24 +0200

    change try-catch to then-catch

--- src/api.js
+++ src/api.js
@@ -10,45 +10,31 @@ const app = express()
 dotenv.config()
 const { PORT = 4001, MONGODB_URI } = process.env
 
-const start = async () => {
-	try {
-		await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
+mongoose
+	.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
 		console.log('✅ Successfully connected to MongoDB')
 		app.listen(PORT, () => console.log(`✅ Server is running on port ${PORT}`))
-	} catch (err) {
+	})
+	.catch(err => {
 		console.log('❌ Error:', err)
 		process.exit(1)
-	}
-
-	// mongoose
-	// 	.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-	// 	.then(() => {
-	// 		console.log('✅ Successfully connected to MongoDB')
-	// 		app.listen(PORT, () => console.log(`✅ Server is running on port ${PORT}`))
-	// 	})
-	// 	.catch(err => {
-	// 		console.log('❌ Error:', err)
-	// 		process.exit(1)
-	// 	})
-
-	app.use(cors())
-	app.use(express.json())
-
-	const NETLIFY_BASE_URL = '/.netlify/functions/api'
-
-	app.use(`${NETLIFY_BASE_URL}/chats`, controllers.chats)
-	app.use(`${NETLIFY_BASE_URL}/tsb-stores`, controllers.stores)
-
-	app.use('*', (req, res) => {
-		res.status(400).send({ message: "This endpoint isn't correct" })
 	})
 
-	app.use((err, req, res, next) => {
-		res.status(err.status || 500).send({ message: err.message })
-	})
-}
+app.use(cors())
+app.use(express.json())
 
-start()
+const NETLIFY_BASE_URL = '/.netlify/functions/api'
+
+app.use(`${NETLIFY_BASE_URL}/chats`, controllers.chats)
+app.use(`${NETLIFY_BASE_URL}/stores`, controllers.stores)
+
+app.use('*', (req, res) => {
+	res.status(400).send({ message: "This endpoint isn't correct" })
+})
+
+app.use((err, req, res, next) => {
+	res.status(err.status || 500).send({ message: err.message })
+})
 
 module.exports.handler = serverless(app)
