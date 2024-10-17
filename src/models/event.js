8eab31192f8dca6697bbe723c24d4976dadb1727
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-10-17 16:16:30 +0300

    add location to event model

--- src/models/event.js
+++ src/models/event.js
@@ -3,10 +3,12 @@ const { Schema, model } = require('mongoose')
 const schema = new Schema(
 	{
 		title: { type: String, required: true }, // "Футбол Броварська"
+		description: { type: String, default: null },
 		creatorUsername: { type: String, default: null }, // "kozubskyi"
 		chatId: { type: Number, required: true }, // 123456789
 		// type: { type: String, enum: ['single event', 'regular event'], default: 'single event' }, // "single event"
 		// days: { type: [String], enum: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Нд'], default: null }, // ["Пн", "Чт"]
+		location: { type: String, default: null }, // Броварська
 		start: { type: String, required: true }, // "07.10.2024, 20:30"
 		end: { type: String, default: null }, // "07.10.2024, 22:00" or null
 		registrationStart: { type: String, default: null }, // "07.10.2024, 09:00" or null
@@ -15,7 +17,6 @@ const schema = new Schema(
 		participantsMin: { type: Number, default: null }, // 10 or null
 		participantsMax: { type: Number, default: null }, // 15 or null
 		participants: { type: [String], default: [] },
-		description: { type: String, default: null },
 	},
 	{ versionKey: false }
 )
