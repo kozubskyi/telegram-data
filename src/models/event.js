afdd7f999e0996a8271d8d246347afa4c22227aa
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2024-10-18 14:43:11 +0300

    add chatTitle

--- src/models/event.js
+++ src/models/event.js
@@ -2,13 +2,14 @@ const { Schema, model } = require('mongoose')
 
 const schema = new Schema(
 	{
-		title: { type: String, required: true }, // "Футбол Броварська"
-		description: { type: String, default: null },
+		title: { type: String, required: true }, // "Футбол"
+		description: { type: String, default: null }, // "Формат 5х5, м'яч 4-ка"
 		creatorUsername: { type: String, default: null }, // "kozubskyi"
+		chatTitle: { type: String, default: null }, // "Me and bots"
 		chatId: { type: Number, required: true }, // 123456789
 		// type: { type: String, enum: ['single event', 'regular event'], default: 'single event' }, // "single event"
 		// days: { type: [String], enum: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Нд'], default: null }, // ["Пн", "Чт"]
-		location: { type: String, default: null }, // Броварська
+		location: { type: String, default: null }, // "Броварська"
 		start: { type: String, required: true }, // "07.10.2024, 20:30"
 		end: { type: String, default: null }, // "07.10.2024, 22:00" or null
 		registrationStart: { type: String, default: null }, // "07.10.2024, 09:00" or null
